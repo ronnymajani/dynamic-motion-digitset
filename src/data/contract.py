7ad282bed97d297d103779da8716f7e19428108b
Ronny Majani <ronnymajani@gmail.com>
2017-12-02 19:14:30 +0300

    Added Intial Files for Main Project

---
+++ src/data/contract.py
@@ -0,0 +1,10 @@
+import json
+
+
+class DataSetContract(object):
+    VERSION = "0.2"
+
+    METADATA = ""
+
+    def __init__(self):
+        pass
---
+++ src/main.py
@@ -0,0 +1,6 @@
+import data.contract
+import data.interface
+
+
+if __name__ == "__main__":
+    print("Hello")
