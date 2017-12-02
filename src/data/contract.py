6b2c212585c9f35e97230539207fad95a39b56f8
Ronny Majani <ronnymajani@gmail.com>
2017-12-02 19:26:52 +0300

    Created Data Format Contract class

--- src/data/contract.py
+++ src/data/contract.py
@@ -1,10 +1,26 @@
-import json
-
-
-class DataSetContract(object):
+class DataSetContract:
     VERSION = "0.2"
+    METADATA = "dataset_metadata"
+    DIGITSETS = "digitsets"
 
-    METADATA = ""
+    class Metadata:
+        MISC = "misc"
 
-    def __init__(self):
-        pass
+        class Device:
+            NAME = "device_name"
+            SAMPLING_RATE = "sampling_rate"
+            RESOLUTION = "device_resolution"
+            PEN_PRESSURE = "pressure_resolution"
+
+            RESOLUTION_WIDTH = "W"
+            RESOLUTION_HEIGHT = "H"
+
+    class DigitSets:
+        METADATA = "digitset_metadata"
+        DIGITS = "digits"
+        Digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+        class Metadata:
+            USER_AGE = "user_age"
+            USER_SEX = "user_gender"
+            USER_HAND = "user_hand"
